36ebe9ee0ff776de1835ebd85f0702832bea756d
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-04-07 10:07:25 +0000

    Cambie nombre dir

--- p07_parcial_1/src/algoritmos_min_TCT/GRASP/busquedaLocal/movimientoSwapInterGRASP.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../algoritmo_GRASP_TCT.h"
-
-class MovimientoSwapInterGRASP : public AlgoritmoGRASP {
- private:
-  virtual const std::vector<Solucion> busquedaLocal(const std::vector<Solucion>&, const Nodo*) const override;
-  const int calcularTCTParcial(const Nodo*, const int, const int, const Solucion&, Solucion&) const;
-};
